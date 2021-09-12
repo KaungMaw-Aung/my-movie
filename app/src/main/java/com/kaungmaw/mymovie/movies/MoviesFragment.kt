9796a5574312d79a5f8c5541b5a8df4e781a6131
Kaung Maw Aung <kaungmawaung99@gmail.com>
2021-09-12 11:11:42 +0630

    - added Lottie animation library - show loading state during fetching movies

--- app/src/main/java/com/kaungmaw/mymovie/movies/MoviesFragment.kt
+++ app/src/main/java/com/kaungmaw/mymovie/movies/MoviesFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.kaungmaw.mymovie.R
 import com.kaungmaw.mymovie.databinding.FragmentMoviesBinding
 import kotlinx.coroutines.flow.collectLatest
@@ -29,6 +31,18 @@ class MoviesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val adapter = MoviesAdapter()
+
+        adapter.addLoadStateListener { loadState ->
+            binding.rvMovies.isVisible = loadState.source.refresh is LoadState.NotLoading
+            if (loadState.source.refresh is LoadState.Loading) {
+                binding.lavLoadState.visibility = View.VISIBLE
+                binding.lavLoadState.playAnimation()
+            } else {
+                binding.lavLoadState.cancelAnimation()
+                binding.lavLoadState.visibility = View.INVISIBLE
+            }
+        }
+
         binding.rvMovies.adapter = adapter
 
         lifecycleScope.launch {
